cfde83d93aa22d53fe27ef15f1c507d2951b733e
dujiaxin <fashui01@163.com>
2019-12-12 18:35:34 -0500

    Feature: add blueprint in app

--- src/app.py
+++ src/app.py
@@ -5,7 +5,9 @@ from flask import Flask
 from .config import app_config
 from .models import db, bcrypt
 
-
+# import user_api blueprint
+from .views.UserView import user_api as user_blueprint
+from .views.BlogpostView import blogpost_api as blogpost_blueprint
 
 
 def create_app(env_name):
@@ -22,7 +24,8 @@ def create_app(env_name):
   bcrypt.init_app(app)
   db.init_app(app)
 
-
+  app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+  app.register_blueprint(blogpost_blueprint, url_prefix='/api/v1/blogposts')
 
   @app.route('/', methods=['GET'])
   def index():
