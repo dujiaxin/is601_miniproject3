de331098c67de0a30e5f6b654d98f983e95d082e
dujiaxin <fashui01@163.com>
2019-12-12 17:03:18 -0500

    Feature: Flask first entry

---
+++ run.py
@@ -0,0 +1,14 @@
+# /run.py
+import os
+
+
+from src.app import create_app
+
+load_dotenv(find_dotenv())
+
+
+
+if __name__ == '__main__':
+  env_name = os.getenv('FLASK_ENV')
+  app = create_app(env_name)
+  app.run()
---
+++ src/app.py
@@ -0,0 +1,32 @@
+#src/app.py
+
+from flask import Flask
+
+from .config import app_config
+from .models import db, bcrypt
+
+# import user_api blueprint
+from .views.UserView import user_api as user_blueprint
+from .views.BlogpostView import blogpost_api as blogpost_blueprint
+
+
+def create_app(env_name):
+  """
+  Create app
+  """
+  
+  # app initiliazation
+  app = Flask(__name__)
+
+  app.config.from_object(app_config[env_name])
+
+
+  @app.route('/', methods=['GET'])
+  def index():
+    """
+    example endpoint
+    """
+    return 'Congratulations! Your first endpoint is working'
+
+  return app
+
