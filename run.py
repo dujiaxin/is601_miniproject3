7f9a63d3836b4db28007346ee339800a628687b8
dujiaxin <fashui01@163.com>
2019-12-12 20:10:09 -0500

    Task: set up for heroku

--- run.py
+++ run.py
@@ -1,14 +1,11 @@
 # /run.py
 import os
-
-
 from src.app import create_app
 
-load_dotenv(find_dotenv())
-
-
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
 
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
-  app.run()
+    port = os.getenv('PORT')
+    # run app
+    app.run(host='0.0.0.0', port=port)
